caf701cd008c3a93b57228f4c5f1e7d4c4d287b0
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-07-08 12:14:11 +0100

    setup file structure for inspiration database, scripts for setting up database, and set up dotenv & express as dependencies and pg & jest as dev dependency

---
+++ __tests__/seed.test.js
@@ -0,0 +1,2 @@
+import runSeed from "../src/server/db/run-seed";
+import seed from "../src/server/db/seed";
---
+++ src/server/db/setup-dbs.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS inspiration_station;
+CREATE DATABASE inspiration_station;
