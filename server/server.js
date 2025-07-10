b203ba72b40af2066c1e5dcdbc9a6fc1a08700eb
parrais <github@parrais.co.uk>
2025-07-10 16:28:55 +0100

    WIP start work on socket.io room functionality

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import { createServer } from "node:http";
-import { Server } from "socket.io";
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
@@ -13,29 +13,48 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: 'http://localhost:5173',
   },
 });
 
 const liveUsers = new Set();
-let linesHistory = [];
+const linesHistory = [];
 
+const knownRooms = ['123XYZ']; // temp room for test purposes
+const knownCanvases = {};
 //storedCanvases = {#socket.id1 : [lines],
 //                  #socket.id2 : [lines]}
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-  socket.on("get-initial-canvas", () => {
-    socket.emit("initial-canvas", linesHistory);
+  socket.on('joinRoomRequest', (roomId) => {
+    if (roomExists(roomId)) {
+      socket.join(roomId);
+      socket.emit('roomJoined', roomId);
+      if (!roomId in knownCanvases) {
+        knownCanvases[roomId] = [];
+      }
+      socket.emit('initial-canvas', knownCanvases[roomId]);
+    } else {
+      socket.emit('roomJoinError', 'Room ID not recognised');
+    }
+  });
+
+  function roomExists(roomId) {
+    return knownRooms.includes(roomId);
+  }
+
+  socket.on('get-initial-canvas', () => {
+    socket.emit('initial-canvas', linesHistory);
   });
 
   liveUsers.add(socket.id);
 
-  socket.on("test-socket", (testMsg) => {
-    io.emit("test message received", testMsg + "<-- server response");
+  socket.on('test-socket', (testMsg) => {
+    io.emit('test message received', testMsg + '<-- server response');
 
-    console.log("Server response: " + testMsg);
+    console.log('Server response: ' + testMsg);
   });
 
   // socket.on("drawing", (data) => {
@@ -45,19 +64,19 @@ io.on("connection", (socket) => {
 
   //try emitting just the new line (amended receptio on line 38 of the KonvaCanvas component)
 
-  socket.on("drawing", (data) => {
+  socket.on('drawing', (data) => {
     if (!data) return;
     linesHistory.push(data);
-    io.emit("drawing", data);
+    io.emit('drawing', data);
   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
     liveUsers.delete(socket.id);
-    io.emit("live-users", Array.from(liveUsers));
+    io.emit('live-users', Array.from(liveUsers));
   });
 
-  io.emit("live-users", Array.from(liveUsers));
+  io.emit('live-users', Array.from(liveUsers));
 });
 
 const PORT = process.env.PORT || 5174;
