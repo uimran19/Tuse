b5db3e766f7dabfb56466a9fd3cea183a5cf1e18
charliewturner <c.w.turner@hotmail.com>
2025-07-10 11:07:09 +0100

    WIP - Move tested frontend files into main repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Canvas from './components/Canvas'
-
+import { useState } from "react";
+import "./App.css";
+import Canvas from "./components/Canvas";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Canvas />
+      <Routes>
+        <Route path="/canvas" element={<Canvas />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,66 +1,158 @@
-import { useState, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Stage, Layer, Rect, Circle, Line, Text } from "react-konva";
+import { socket } from "../socket";
 
-function Canvas() {
-  const [tool, setTool] = useState("pen");
-  const [lines, setLines] = useState([]);
-  const isDrawing = useRef(false);
+const Canvas = () => {
+  const [tool, setTool] = React.useState("pen");
+  const [lines, setLines] = React.useState([]);
+  const [liveLine, setLiveLine] = React.useState(null);
+  const isDrawing = React.useRef(false);
+  const [storedCanvas, setStoredCanvas] = React.useState(null);
+  let socketIdRef = useRef("");
+  const [liveUsers, setLiveUsers] = React.useState([]);
+  const stageRef = useRef(null);
 
   const handleMouseDown = (e) => {
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
-    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    setLiveLine({ tool, points: [pos.x, pos.y], socketIdRef });
   };
 
+  useEffect(() => {
+    socket.on("initial-canvas", (linesHistory) => {
+      setLines(linesHistory);
+    });
+
+    socket.on("live-users", (currUsers) => {
+      setLiveUsers(currUsers);
+    });
+
+    socket.on("test message received", (testMsg) => {
+      console.log(testMsg);
+    });
+
+    // socket.on("drawing", (linesHistory) => {
+    //   setLines(linesHistory);
+    // });
+
+    socket.on("drawing", (newLine) => {
+      setLines((previous) => [...previous, newLine]);
+    });
+
+    socket.on("connect", () => {
+      socketIdRef.current = socket.id;
+      socket.emit("get-initial-canvas");
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("live-users");
+      socket.off("test message received");
+      socket.off("drawing");
+      socket.off("initial-canvas");
+    };
+  }, []);
+
   const handleMouseMove = (e) => {
-    // no drawing - skipping
     if (!isDrawing.current) {
       return;
     }
     const stage = e.target.getStage();
     const point = stage.getPointerPosition();
-    let lastLine = lines[lines.length - 1];
-    // add point
-    lastLine.points = lastLine.points.concat([point.x, point.y]);
 
-    // replace last
-    lines.splice(lines.length - 1, 1, lastLine);
-    setLines(lines.concat());
+    setLiveLine({
+      ...liveLine,
+      points: [...liveLine.points, point.x, point.y],
+    });
   };
 
   const handleMouseUp = () => {
     isDrawing.current = false;
+    socket.emit("drawing", liveLine);
+    console.log(lines);
+
+    //prevents flickering of liveLine
+    requestAnimationFrame(() => {
+      setLiveLine(null);
+    });
+  };
+
+  const handleExport = () => {
+    const dataURL = stageRef.current.toDataURL({
+      pixelRatio: 2, // double resolution
+    });
+
+    const link = document.createElement("a");
+    link.download = "stage.png";
+    link.href = dataURL;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={handleMouseDown}
-      onMousemove={handleMouseMove}
-      onMouseup={handleMouseUp}
-      onTouchStart={handleMouseDown}
-      onTouchMove={handleMouseMove}
-      onTouchEnd={handleMouseUp}
-    >
-      <Layer>
-        {lines.map((line, i) => (
-          <Line
-            key={i}
-            points={line.points}
-            stroke="#df4b26"
-            strokeWidth={5}
-            tension={0.5}
-            lineCap="round"
-            lineJoin="round"
-            globalCompositeOperation={
-              line.tool === "eraser" ? "destination-out" : "source-over"
-            }
-          />
-        ))}
-      </Layer>
-    </Stage>
+    <div>
+      <button onClick={handleExport}>Download</button>
+      <select
+        value={tool}
+        onChange={(e) => {
+          setTool(e.target.value);
+        }}
+      >
+        <option value="pen">Pen</option>
+        <option value="eraser">Eraser</option>
+      </select>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
+        // onMouseOut={handleMouseUp}
+        onTouchStart={handleMouseDown}
+        onTouchMove={handleMouseMove}
+        onTouchEnd={handleMouseUp}
+        ref={stageRef}
+        onClick={(e) => {
+          const stage = e.target.getStage();
+
+          //   console.log(stage);
+          setStoredCanvas(stage);
+        }}
+      >
+        <Layer>
+          <Text text="Just start drawing" x={5} y={30} />
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="#df4b26"
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+              lineJoin="round"
+              globalCompositeOperation={
+                line.tool === "eraser" ? "destination-out" : "source-over"
+              }
+            />
+          ))}
+          {liveLine && (
+            <Line
+              points={liveLine.points}
+              stroke="#df4b26"
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+              lineJoin="round"
+              globalCompositeOperation={
+                liveLine.tool === "eraser" ? "destination-out" : "source-over"
+              }
+            />
+          )}
+        </Layer>
+      </Stage>
+    </div>
   );
-}
+};
 
 export default Canvas;
