86435b31e8b55924c8fc7073322bbd49eded726b
Ruby Kwong <ruby.kwong@btinternet.com>
2025-07-11 10:54:11 +0100

    link up fetchInspiration model and getTodaysInspiration controller

--- server/controllers/getTodaysInspiration.js
+++ server/controllers/getTodaysInspiration.js
@@ -1,4 +1,5 @@
 const selectInspirationData = require("../models/selectInspirationData");
+const fetchInspiration = require("../models/fetchInspiration");
 const { getTodaysDate } = require("../utils");
 
 function getTodaysInspiration(req, res, next) {
@@ -6,6 +7,11 @@ function getTodaysInspiration(req, res, next) {
   return selectInspirationData(todaysDate)
     .then(({ rows: [{ artwork_id, image_id }] }) => {
       return { artwork_id, image_id };
+    }).then(({artwork_id, image_id}) => {
+      return fetchInspiration(artwork_id, image_id)
+    }).then((artwork) => {
+      return artwork
+      // response.status(200).send({artwork})
     })
     .catch((err) => {
       console.log(err);
--- server/models/fetchInspiration.js
+++ server/models/fetchInspiration.js
@@ -1,17 +1,15 @@
 const format = require("pg-format");
 const db = require("../db/connection");
 
-function fetchInspiration(artwork_id, image_id, size){
+function fetchInspiration(artwork_id, image_id){
     return fetch(`https://api.artic.edu/api/v1/artworks/${artwork_id}/manifest.json`)
     .then((res) => {
         return res.json()
-    }).then((res)=>{
-        res.thumbnailUrl = `https://www.artic.edu/iiif/2/${image_id}/full/400,/0/default.jpg`
-        res.imageUrl = `https://www.artic.edu/iiif/2/${image_id}/full/1686,/0/default.jpg`
-        return 
-        {res}
+    }).then((artwork)=>{
+        artwork.thumbnailUrl = `https://www.artic.edu/iiif/2/${image_id}/full/400,/0/default.jpg`
+        artwork.imageUrl = `https://www.artic.edu/iiif/2/${image_id}/full/1686,/0/default.jpg`
+        return artwork
     })
 }
 
-fetchInspiration(28560)
 module.exports = fetchInspiration
