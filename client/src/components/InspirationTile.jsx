33a8a3d703626f0c319816c15e43c50624d8c17c
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-07-11 15:08:23 +0100

    modify InspirationTile to make call to api within useEffect and assign inspiration response to stateful variable

--- client/src/components/InspirationTile.jsx
+++ client/src/components/InspirationTile.jsx
@@ -1,15 +1,27 @@
 import styled from "styled-components";
 import { getInspirationMetaData } from "../api";
 import Tile from "./Tile";
+import { useEffect, useState } from "react";
 
 export default function InspirationTile({ date }) {
+  const [inspiration, setInspiration] = useState({});
+
+  useEffect(() => {
+    let {
+      inspiration: { thumbnailUrl, label, artist_title },
+    } = getInspirationMetaData(date);
+    setInspiration(inspiration);
+  }, []);
+
   const testMetaData = {
     thumbnailUrl:
       "https://www.artic.edu/iiif/2/25c31d8d-21a4-9ea1-1d73-6a2eca4dda7e/full/400,/0/default.jpg",
     label: "The Bedroom",
     artist_title: "Vincent Van Gogh",
   };
-  const { thumbnailUrl, label, artist_title } = testMetaData; // getInspirationMetaData(date);
+  let {
+    inspiration: { thumbnailUrl, label, artist_title },
+  } = testMetaData;
 
   return (
     <Tile src={thumbnailUrl}>
