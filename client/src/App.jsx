d06f31bf1465a2a1d213058b4e2b464b1439a293
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-07-11 10:46:28 +0100

    add HomePage route to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Canvas from "./components/Canvas";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/canvas" element={<Canvas />}></Route>
       </Routes>
     </>
