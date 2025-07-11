04bf9cd5fb93ff876a706006f3f3fcc11df574de
charliewturner <c.w.turner@hotmail.com>
2025-07-11 14:05:46 +0100

    Change CORS allowed origins. Add type:module into package.json

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://tusemain.netlify.app/canvas/XYZ123",
+      "http://127.0.0.1:5173",
+    ],
   },
 });
 
