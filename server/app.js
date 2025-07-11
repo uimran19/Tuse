cf370c9de3bccd2f3f01d90d37ad1e7adc0cd68a
Ruby Kwong <ruby.kwong@btinternet.com>
2025-07-11 14:09:49 +0100

    add listen file, set main to listen.js and add scripts to package.json, and fix minor bug in controller function

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const getTodaysInspiration = require("./controllers/getTodaysInspiration");
 
 app.use(cors());
 
